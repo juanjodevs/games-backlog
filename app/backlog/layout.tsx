a6c768a3dab23e9e80d006238053e1bad472f0a2
Juanjo <juanjodevs@gmail.com>
2025-02-22 23:15:53 +0100

    Changed header text and bg color

--- app/backlog/layout.tsx
+++ app/backlog/layout.tsx
@@ -7,12 +7,12 @@ export default function BacklogLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="relative min-h-screen bg-gradient-to-b from-slate-900 to-slate-800">
+    <div className="relative min-h-screen bg-gradient-to-b from-slate-900 to-slate-600">
       <header className="sticky top-0 z-50 border-b border-border/40 bg-slate-900/95 backdrop-blur supports-[backdrop-filter]:bg-slate-900/75">
         <div className="container flex h-14 max-w-7xl items-center mx-auto">
           <div className="mr-4 flex">
             <a className="mr-6 flex items-center space-x-2 text-white" href="/backlog">
-              <span className="hidden font-bold sm:inline-block text-4xl">What To Play Next?</span>
+              <span className="hidden font-bold sm:inline-block text-4xl">GamesBacklog</span>
             </a>
           </div>
           <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
